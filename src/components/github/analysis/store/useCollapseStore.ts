72041785a088fff761c027db344ddd1524a08299
qinscode <fudong.qin@outlook.com>
2025-05-19 16:06:34 +0800

    chore: update version number to v0.2.7
    - Update the version number in the GitHub Repository Analyzer application
    - This change reflects the current version of the application as v0.2.7

--- src/components/github/analysis/store/useCollapseStore.ts
+++ src/components/github/analysis/store/useCollapseStore.ts
@@ -1,13 +1,17 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface CollapseState {
-  isAllExpanded: boolean;
-  toggleAll: () => void;
-  setExpanded: (expanded: boolean) => void;
+	isAllExpanded: boolean;
+	toggleAll: () => void;
+	setExpanded: (expanded: boolean) => void;
 }
 
 export const useCollapseStore = create<CollapseState>((set) => ({
-  isAllExpanded: false,
-  toggleAll: () => set((state) => ({ isAllExpanded: !state.isAllExpanded })),
-  setExpanded: (expanded: boolean) => set({ isAllExpanded: expanded }),
-})); 
+	isAllExpanded: false,
+	toggleAll: () => {
+		set((state) => ({ isAllExpanded: !state.isAllExpanded }));
+	},
+	setExpanded: (expanded: boolean) => {
+		set({ isAllExpanded: expanded });
+	},
+}));
